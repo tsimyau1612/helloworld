23195b2375a71de30fcc033b4826115a1d60694e
Tsim Yau <tsim_yau@yahoo.co.id>
2018-12-28 17:36:00 +0700

    helloworld pip package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='helloworld',
+      version='0.1',
+      description='The package that prints Hello World',
+      url='https://github.com/tsimyau1612/helloworld',
+      author='tsimyau1612',
+      author_email='tsimyau1612@gmail.com',
+      packages=['helloworld'],
+      zip_safe=False)
